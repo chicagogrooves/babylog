b5b7446daceb67a4811a173cf2c0b6573dc892a1
Dean Radcliffe <dradcliffe@opinionlab.com>
2014-12-14 22:38:06 -0600

    L R buttons styled, reactive

--- client/feeding.next.js
+++ client/feeding.next.js
@@ -3,6 +3,11 @@ Template.feeding.rendered = () => {
         Session.set("feeding", Feedings.insert({time: new Date()}))
     }
 }
+
+Template.feeding.helpers({
+    active: function (which) {return this[which] ? "active" : ""}
+})
+
 Template.feeding.events({
     "click #btn-end": (e, t) => {
         Feedings.update(t.data._id, {$set: {completed: true}})
@@ -12,6 +17,7 @@ Template.feeding.events({
     "click .which": (e, t) => {
         let obj = {},
             which = e.target.attributes["data-which"].value
+
         obj[which] = true
         Feedings.update(t.data._id, {$set: obj})
     }
