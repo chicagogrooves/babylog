828e4e9592df258fa4aea130eb1a5a1ff6ae188b
Dean Radcliffe <dradcliffe@opinionlab.com>
2014-12-14 22:47:09 -0600

    begun re-caffienating
    es6 syntax ain’t ringin my bells

---
+++ client/feeding.coffee
@@ -0,0 +1,21 @@
+Template.feeding.rendered = ->
+  if !Session.get("feeding")
+    Session.set "feeding", Feedings.insert
+      time: new Date()
+
+Template.feeding.helpers
+  active: (which) ->
+    if @[which] then "active" else ""
+
+Template.feeding.events
+  "click #btn-end": (e, t) ->
+    Feedings.update t.data._id, $set: {completed: true}
+    Session.set "feeding", undefined
+    Router.go "history"
+
+  "click .which": (e, t) ->
+    obj = {}
+    which = e.target.attributes["data-which"].value
+    obj[which] = true
+    Feedings.update t.data._id, $set: obj
+    e.target.blur()
--- client/feeding.next.js
+++ client/feeding.next.js
@@ -1,24 +1,24 @@
-Template.feeding.rendered = () => {
-    if(!Session.get("feeding")){
-        Session.set("feeding", Feedings.insert({time: new Date()}))
-    }
-}
-
-Template.feeding.helpers({
-    active: function (which) {return this[which] ? "active" : ""}
-})
-
-Template.feeding.events({
-    "click #btn-end": (e, t) => {
-        Feedings.update(t.data._id, {$set: {completed: true}})
-        Session.set("feeding")
-        Router.go("history")
-    },
-    "click .which": (e, t) => {
-        let obj = {},
-            which = e.target.attributes["data-which"].value
-
-        obj[which] = true
-        Feedings.update(t.data._id, {$set: obj})
-    }
-})
+// Template.feeding.rendered = () => {
+//     if(!Session.get("feeding")){
+//         Session.set("feeding", Feedings.insert({time: new Date()}))
+//     }
+// }
+//
+// Template.feeding.helpers({
+//     active: function (which) {return this[which] ? "active" : ""}
+// })
+//
+// Template.feeding.events({
+//     "click #btn-end": (e, t) => {
+//         Feedings.update(t.data._id, {$set: {completed: true}})
+//         Session.set("feeding")
+//         Router.go("history")
+//     },
+//     "click .which": (e, t) => {
+//         let obj = {},
+//             which = e.target.attributes["data-which"].value
+//
+//         obj[which] = true
+//         Feedings.update(t.data._id, {$set: obj})
+//     }
+// })
